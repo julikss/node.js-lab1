2aea49a2364c62791a51a84d2b4553d414b3c08b
Anastasiia Sanina <saninaanstasia97@gmail.com>
2023-02-16 01:46:50 +0200

    Delete map

--- duplicateEncode.js
+++ duplicateEncode.js
@@ -1,6 +1,6 @@
 const duplicateEncode = (word) => {
-  let modified = word.split('').map(el => el.toLowerCase());
-  let duplicates = modified.filter((el, index) =>index !== modified.indexOf(el));
+  let modified = word.toLowerCase().split('');
+  let duplicates = modified.filter((el, index) => index !== modified.indexOf(el));
   
   return modified.map(el => {
     if(duplicates.includes(el)) return ')'
